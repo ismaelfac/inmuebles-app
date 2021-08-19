c4ee5f938aec2f92a8a77eed218e28c73f3b1c0e
ISMAEL E. LASTRE ALVAREZ <ismaelfac@gmail.com>
2021-08-19 13:21:02 -0500

    Module Inventary

--- src/app/components/dashboard/inventory/home-inventory/home-inventory.component.ts
+++ src/app/components/dashboard/inventory/home-inventory/home-inventory.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-inventory.component.css']
 })
 export class HomeInventoryComponent implements OnInit {
-
+  moduleActive : boolean = false;
+  panel_title: string = 'Panel de Inventarios';
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  setModuleActive($event:any): void {
+    console.log($event);
+    this.panel_title = $event;
+    this.moduleActive = true;
   }
 
 }
