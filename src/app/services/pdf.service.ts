0ba875becf5cae6bba3bf46df9450fa9ce6ce89d
ISMAEL E. LASTRE ALVAREZ <ismaelfac@gmail.com>
2021-09-13 12:03:45 -0500

    json contractIndication

--- src/app/services/pdf.service.ts
+++ src/app/services/pdf.service.ts
@@ -14,6 +14,15 @@ export class PdfService {
   }
 
   setHeader(pdf: any, contract: any) {
+    pdf.styles({
+        title: {
+            bold: true,
+            'align-items': 'center'
+        },
+        style2: {
+            italics: true
+        }
+    });
     pdf.info({
       title: `Contrato No ${contract[0].contractId}`,
       author: 'Aliados Inmobiliarios s.a',
@@ -24,7 +33,7 @@ export class PdfService {
   setArrendatario(pdf: any, contract: any) {
     new Columns([ `Arrendatario: ${contract[0].contractId}`,
                    `Identificación: ${contract[0].contractId}`,
-                  `RUT:` ]).end
+                  `RUT:` ]).style('title').end
   }
   
   generatePdf(contract: any) {
@@ -32,20 +41,26 @@ export class PdfService {
     this.setHeader(pdf, contract);
     pdf.header('Aliados Inmobiliarios sa');
     pdf.rawContent([
-        new Columns([
-          {
-            width: '40%',
-            text: `Arrendatario: ${contract[0].id }`
-          },
-          {
-            width: '30%',
-            text: `Arrendatario: ${contract[0].contractId }`
-          },
-          {
-            width: '30%',
-            text: `Arrendatario: ${contract[0].contractId }`
-          },
-        ]).columnGap(10).bold().end,
+      new Columns([
+        {
+          width: '100%',
+          text: `Indicaciones de Contrato No: ${contract[0].id }`
+        }
+      ]).columnGap(10).bold().alignment('center').end,
+      new Columns([
+        {
+          width: '40%',
+          text: `Arrendatario: ${contract[0].id }`
+        },
+        {
+          width: '30%',
+          text: `Arrendatario: ${contract[0].contractId }`
+        },
+        {
+          width: '30%',
+          text: `Arrendatario: ${contract[0].contractId }`
+        },
+      ]).columnGap(10).bold().end,
         new Table([
           [ 'column 1', 'column 2'],
           ['',''],
@@ -54,12 +69,16 @@ export class PdfService {
           [ 'column 1', 'column 2'],
           ['',''],
           [ 'column 1', 'column 2']
-        ]).widths([ '*', 100 ]).end,
+        ]).widths([ '*', 80 ]).end,
         new Columns([ `Arrendatario: ${contract[0].CheckListContract[0].arrendatario }`,
                      `Identificación: ${contract[0].CheckListContract[0].PersonId }`,
                      `RUT:` ]).end
     ]);
-    
+    let arrendatarioLength = contract[0].CheckListContract.length;
+    for (let arrendatario = 0; arrendatario <= arrendatarioLength; arrendatario++) {
+      const element = arrendatarioLength[arrendatario];
+      console.log(element);      
+    }
     pdf.create().open();
     }
 
